7ae32c28dd12324542e7c7806bfe7c1019b95b10
Salehen Rahman <salehen.rahman@gmail.com>
2012-09-07 12:47:26 -0700

    The content no longer clears away whenever the users hits escape.

--- src/jquery-ui.inlinedAutocomplete.js
+++ src/jquery-ui.inlinedAutocomplete.js
@@ -28,8 +28,192 @@
 			delay: 10
 		},
 
-		_create:function() {
+	    __create: function() {
+	        var self = this,
+	            doc = this.element[ 0 ].ownerDocument,
+	            suppressKeyPress;
+	        this.isMultiLine = this.element.is( "textarea" );
 
+	        this.element
+	            .addClass( "ui-autocomplete-input" )
+	            .attr( "autocomplete", "off" )
+	            // TODO verify these actually work as intended
+	            .attr({
+	                role: "textbox",
+	                "aria-autocomplete": "list",
+	                "aria-haspopup": "true"
+	            })
+	            .bind( "keydown.autocomplete", function( event ) {
+	                if ( self.options.disabled || self.element.propAttr( "readOnly" ) ) {
+	                    return;
+	                }
+
+	                suppressKeyPress = false;
+	                var keyCode = $.ui.keyCode;
+	                switch( event.keyCode ) {
+	                case keyCode.PAGE_UP:
+	                    self._move( "previousPage", event );
+	                    break;
+	                case keyCode.PAGE_DOWN:
+	                    self._move( "nextPage", event );
+	                    break;
+	                case keyCode.UP:
+	                    self._keyEvent( "previous", event );
+	                    break;
+	                case keyCode.DOWN:
+	                    self._keyEvent( "next", event );
+	                    break;
+	                case keyCode.ENTER:
+	                case keyCode.NUMPAD_ENTER:
+	                    // when menu is open and has focus
+	                    if ( self.menu.active ) {
+	                        // #6055 - Opera still allows the keypress to occur
+	                        // which causes forms to submit
+	                        suppressKeyPress = true;
+	                        event.preventDefault();
+	                    }
+	                    //passthrough - ENTER and TAB both select the current element
+	                case keyCode.TAB:
+	                    if ( !self.menu.active ) {
+	                        return;
+	                    }
+	                    self.menu.select( event );
+	                    break;
+	                case keyCode.ESCAPE:
+	                    //self.element.val( self.term );
+	                    self.close( event );
+	                    break;
+	                default:
+	                    // keypress is triggered before the input value is changed
+	                    clearTimeout( self.searching );
+	                    self.searching = setTimeout(function() {
+	                        // only search if the value has changed
+	                        if ( self.term != self.element.val() ) {
+	                            self.selectedItem = null;
+	                            self.search( null, event );
+	                        }
+	                    }, self.options.delay );
+	                    break;
+	                }
+	            })
+	            .bind( "keypress.autocomplete", function( event ) {
+	                if ( suppressKeyPress ) {
+	                    suppressKeyPress = false;
+	                    event.preventDefault();
+	                }
+	            })
+	            .bind( "focus.autocomplete", function() {
+	                if ( self.options.disabled ) {
+	                    return;
+	                }
+
+	                self.selectedItem = null;
+	                self.previous = self.element.val();
+	            })
+	            .bind( "blur.autocomplete", function( event ) {
+	                if ( self.options.disabled ) {
+	                    return;
+	                }
+
+	                clearTimeout( self.searching );
+	                // clicks on the menu (or a button to trigger a search) will cause a blur event
+	                self.closing = setTimeout(function() {
+	                    self.close( event );
+	                    self._change( event );
+	                }, 150 );
+	            });
+	        this._initSource();
+	        this.menu = $( "<ul></ul>" )
+	            .addClass( "ui-autocomplete" )
+	            .appendTo( $( this.options.appendTo || "body", doc )[0] )
+	            // prevent the close-on-blur in case of a "slow" click on the menu (long mousedown)
+	            .mousedown(function( event ) {
+	                // clicking on the scrollbar causes focus to shift to the body
+	                // but we can't detect a mouseup or a click immediately afterward
+	                // so we have to track the next mousedown and close the menu if
+	                // the user clicks somewhere outside of the autocomplete
+	                var menuElement = self.menu.element[ 0 ];
+	                if ( !$( event.target ).closest( ".ui-menu-item" ).length ) {
+	                    setTimeout(function() {
+	                        $( document ).one( 'mousedown', function( event ) {
+	                            if ( event.target !== self.element[ 0 ] &&
+	                                event.target !== menuElement &&
+	                                !$.ui.contains( menuElement, event.target ) ) {
+	                                self.close();
+	                            }
+	                        });
+	                    }, 1 );
+	                }
+
+	                // use another timeout to make sure the blur-event-handler on the input was already triggered
+	                setTimeout(function() {
+	                    clearTimeout( self.closing );
+	                }, 13);
+	            })
+	            .menu({
+	                focus: function( event, ui ) {
+	                    var item = ui.item.data( "item.autocomplete" );
+	                    if ( false !== self._trigger( "focus", event, { item: item } ) ) {
+	                        // use value to match what will end up in the input, if it was a key event
+	                        if ( /^key/.test(event.originalEvent.type) ) {
+	                            self.element.val( item.value );
+	                        }
+	                    }
+	                },
+	                selected: function( event, ui ) {
+	                    var item = ui.item.data( "item.autocomplete" ),
+	                        previous = self.previous;
+
+	                    // only trigger when focus was lost (click on menu)
+	                    if ( self.element[0] !== doc.activeElement ) {
+	                        self.element.focus();
+	                        self.previous = previous;
+	                        // #6109 - IE triggers two focus events and the second
+	                        // is asynchronous, so we need to reset the previous
+	                        // term synchronously and asynchronously :-(
+	                        setTimeout(function() {
+	                            self.previous = previous;
+	                            self.selectedItem = item;
+	                        }, 1);
+	                    }
+
+	                    if ( false !== self._trigger( "select", event, { item: item } ) ) {
+	                        self.element.val( item.value );
+	                    }
+	                    // reset the term after the select event
+	                    // this allows custom select handling to work properly
+	                    self.term = self.element.val();
+
+	                    self.close( event );
+	                    self.selectedItem = item;
+	                },
+	                blur: function( event, ui ) {
+	                    // don't set the value of the text field if it's already correct
+	                    // this prevents moving the cursor unnecessarily
+	                    if ( self.menu.element.is(":visible") &&
+	                        ( self.element.val() !== self.term ) ) {
+	                        self.element.val( self.term );
+	                    }
+	                }
+	            })
+	            .zIndex( this.element.zIndex() + 1 )
+	            // workaround for jQuery bug #5781 http://dev.jquery.com/ticket/5781
+	            .css({ top: 0, left: 0 })
+	            .hide()
+	            .data( "menu" );
+	        if ( $.fn.bgiframe ) {
+	             this.menu.element.bgiframe();
+	        }
+	        // turning off autocomplete prevents the browser from remembering the
+	        // value when navigating through history, so we re-enable autocomplete
+	        // if the page is unloaded before the widget is destroyed. #7790
+	        self.beforeunloadHandler = function() {
+	            self.element.removeAttr( "autocomplete" );
+	        };
+	        $( window ).bind( "beforeunload", self.beforeunloadHandler );
+	    },
+
+		_create:function() {
 			var self = this;
 			this.id_map = new Object();
 			this.stopIndex = -1;
@@ -42,8 +226,10 @@
 			var id_map_string = this.element.attr('id_map');
 			if(id_map_string) this.id_map = jQuery.parseJSON(id_map_string);
 
-			this.ac = $.ui.autocomplete.prototype;
-			this.ac._create.apply(this, arguments);
+			//this.ac = $.ui.autocomplete.prototype;
+			//this.ac._create.apply(this, arguments);
+
+			this.__create();
 			
 			// Add our custom class here.
 			this.menu.element.addClass('inlined-autocomplete');
