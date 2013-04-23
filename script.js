d2dc3322f82fdf042ff29b97f4614ffff345cb31
domruf <dominikruf@gmail.com>
2013-04-23 14:06:04 +0200

    use load() instead of timeout

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 $(function () {
     $(document).ready(function() {
-        setTimeout(function(){
-            conf = {
-                hidden: '#hidden_inputbox',
-                source: [{
-                        label: 'Lisa Stewart',
-                        value: 'lisa',
-                        //img: 'http://www.gravatar.com/avatar/',
-                        info: 'lisa@somewhere.com'
-                    },
+        conf = {
+            hidden: '#hidden_inputbox',
+            source: [{
+                    label: 'Lisa Stewart',
+                    value: 'lisa',
+                    //img: 'http://www.gravatar.com/avatar/',
+                    info: 'lisa@somewhere.com'
+                },
 
-                    {
-                        label: 'Mike Johnson',
-                        value: 'mike',
-                        img: 'http://www.gravatar.com/avatar/',
-                        info: 'mike@somewhere.com'
-                    },
+                {
+                    label: 'Mike Johnson',
+                    value: 'mike',
+                    img: 'http://www.gravatar.com/avatar/',
+                    info: 'mike@somewhere.com'
+                },
 
-                    {
-                        label: 'Steve Friedman',
-                        value: 'steve',
-                        img: 'http://www.gravatar.com/avatar/',
-                        info: 'steve@somewhere.com'
-                    }],
-                trigger: "@",
-                minLength: 2,
-                maxLength: 25,
-                width: 300
-            }
-            $('#inputbox').inlinedAutocomplete(conf);
-            $('#editable').inlinedAutocomplete(conf);
+                {
+                    label: 'Steve Friedman',
+                    value: 'steve',
+                    img: 'http://www.gravatar.com/avatar/',
+                    info: 'steve@somewhere.com'
+                }],
+            trigger: "@",
+            minLength: 2,
+            maxLength: 25,
+            width: 300
+        }
+        $('#inputbox').inlinedAutocomplete(conf);
+        $('#editable').inlinedAutocomplete(conf);
+        $('#myiframe').load(function(){
             $('#myiframe').contents().find('body#content').inlinedAutocomplete(conf);
             // we don't want the menu be inside the iframe
             $('#myiframe').contents().find('body#content').inlinedAutocomplete('option', 'appendTo', '#iframecontainer');
@@ -39,6 +39,6 @@ $(function () {
             // TODO: put this inside inlinedAutocomplete
             $('#myiframe').contents().find('body#content').inlinedAutocomplete('option', 'offsetLeft', $('#myiframe').offset().left);
             $('#myiframe').contents().find('body#content').inlinedAutocomplete('option', 'offsetTop', $('#myiframe').offset().top);
-        }, 1000);
+        });
     });
 });
